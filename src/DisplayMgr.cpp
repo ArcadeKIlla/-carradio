0d35b093afac89fbfed5f0ae54b6a1f2f66a029c
Nathan Keil <nathanjameskeil@outlook.com>
2025-02-26 10:41:29 -0600

    Add implementation for DisplayMgr::stop() function

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -5002,3 +5002,17 @@ bool DisplayMgr::reset() {
     return false;
 }
 
+void DisplayMgr::stop() {
+	// Turn off LEDs if any
+	LEDeventStop();
+	
+	// If we have a display, clear and release it
+	if (_vfd && _vfd->isSetup()) {
+		_vfd->clearDisplay();
+		_vfd->display();
+	}
+	
+	// Stop any threads or timers if needed
+	_isRunning = false;
+}
+
